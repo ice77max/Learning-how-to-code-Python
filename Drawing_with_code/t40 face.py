05f8e700e0973a5790cf412bcd20e9081a0aa038
ice77max <117373773+ice77max@users.noreply.github.com>
2025-10-28 11:59:48 +0000

    face

---
+++ Drawing_with_code/t40 face.py
@@ -0,0 +1,27 @@
+from turtle import *
+# youtube.com/@sierra_wild.3d
+bgcolor("black")
+
+# face
+dot(600,"yellow")
+# L eye
+teleport(-120,120)
+dot(130,"black")
+# R eye
+teleport(120,120)
+dot(130,"black")
+# mouth
+pensize(15)
+teleport(-140,-100)
+right(75)
+forward(100)
+backward(50)
+
+left(90)
+
+forward(300)
+left(90)
+forward(50)
+backward(100)
+
+exitonclick()
