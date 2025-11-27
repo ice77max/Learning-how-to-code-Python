4181db90e7f39a9d15c897c4d80bcb913bff4026
Pawel <pawelbabiak@gmail.com>
2025-11-27 21:43:45 +0000

    feat: add Hilbert curve drawing implementation using turtle graphics

---
+++ Recursions/Fractals/Hilber Curve.py
@@ -0,0 +1,63 @@
+from turtle import *
+
+# setup
+tracer(10,0)
+setworldcoordinates(0,0, 700, 700)
+hideturtle()
+
+LINE_LENGTH = 5
+ANGLE = 90
+LEVELS = 6
+DRAW_SOLID = False
+setheading(0) # change to start at an angle
+
+# functions
+
+def hilbertCurveQuadrant(level, angle):
+    if level == 0:
+        # Base case
+        return
+    else:
+        # recursive case
+        right(angle)
+        hilbertCurveQuadrant(level - 1, -angle)
+        forward(LINE_LENGTH)
+        left(angle)
+        hilbertCurveQuadrant(level - 1, -angle)
+        forward(LINE_LENGTH)
+        hilbertCurveQuadrant(level - 1, -angle)
+        left(angle)
+        forward(LINE_LENGTH)
+        hilbertCurveQuadrant(level - 1, -angle)
+        left(angle)
+        return
+def hilbertCurve(startingPosition):
+    penup()
+    goto(startingPosition)
+    pendown()
+    if DRAW_SOLID:
+        begin_fill()
+
+    hilbertCurveQuadrant(LEVELS, ANGLE)
+    forward(LINE_LENGTH)
+
+    hilbertCurveQuadrant(LEVELS, ANGLE)
+    left(ANGLE)
+    forward(LINE_LENGTH)
+    left(ANGLE)
+
+    hilbertCurveQuadrant(LEVELS, ANGLE)
+    forward(LINE_LENGTH)
+
+    hilbertCurveQuadrant(LEVELS, ANGLE)
+
+    left(ANGLE)
+    forward(LINE_LENGTH)
+    left(ANGLE)        
+    if DRAW_SOLID:
+        end_fill()
+
+hilbertCurve((30, 350))
+
+
+exitonclick()
