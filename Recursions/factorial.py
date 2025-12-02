4dafe23dbca00bc10e5066abe26e58f84f7590d5
Pawel <pawelbabiak@gmail.com>
2025-12-02 00:22:21 +0000

    feat: add recursive factorial function and performance timing

--- Recursions/factorial.py
+++ Recursions/factorial.py
@@ -1,7 +1,31 @@
+import time
+
+
 def factorial(n):
     product = 1
     for i in range(1, n + 1):
         product = product * i
     return product
 
-print(factorial(5))
+def factorialRecursive(n):
+    if n == 1:
+        # base case
+        return 1
+    else:
+        # recursive case
+        return n * factorialRecursive(n - 1)
+  
+    
+        
+  
+x = 999
+
+start = time.time_ns()
+print(factorial(x))
+end = time.time_ns()
+print(f"It took {end - start} to complete factorial, irritative way")
+
+start = time.time_ns()
+print(factorialRecursive(x))
+end = time.time_ns()
+print(f"It took {end - start} to complete factorial, recursive way")
