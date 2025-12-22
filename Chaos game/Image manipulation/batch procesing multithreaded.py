2294353696e0c44964978082c11567d9359e41ee
Pawel <pawelbabiak@gmail.com>
2025-12-22 19:36:21 +0000

    refactor: update file paths for EPS to JPG conversion process

--- Chaos game/Image manipulation/batch procesing multithreaded.py
+++ Chaos game/Image manipulation/batch procesing multithreaded.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
 # File manipulation
-fileDir = r"D:\Chaos game\50k"
-saveDir = r"D:\Chaos game\50k_jpg"
+fileDir = r"D:\Chaos game\100k"
+saveDir = r"D:\Chaos game\100k_jpg"
 os.makedirs(saveDir, exist_ok=True)
 os.chdir(fileDir) # changes directory
 fileList = os.listdir() # list of files we have in directory 
@@ -19,13 +19,15 @@ def convert_eps(imageFile):
     f, e = os.path.splitext(imageFile)
     
     if e.lower() == ".eps":
+        outFile = f + ".jpg"
+        outPath = os.path.join(saveDir, outFile)
+        if os.path.exists(outPath):
+            return
         try:
             # image manipulation 
             im = Image.open(filePath)
             if im.mode != "RGB":
                 im = im.convert("RGB")
-            outFile = f + ".jpg"
-            outPath = os.path.join(saveDir, outFile)
             im.save(outPath, quality=95, dpi=(300,300))
         except Exception as err:
             print(f"Error converting {imageFile}: {err}")
